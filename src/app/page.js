4c6f6434c635d3750941b8ba8b4d7ff6bf8ce680
MatiRivarola <mati.rivarola15@gmail.com>
2023-10-23 20:33:49 -0300

    First commit.

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,101 @@
-import Image from 'next/image'
+'use client'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="container w-full h-full">
+      <div className=" bg-white">
+        <div className="w-[393px] h-[299px] left-0 top-0 absolute">
+          <img className="w-[393px] h-[239px] left-0 top-[60px] absolute" src="cenafamiliar.jpg" />
+          <div className="w-[393px] h-[61px] left-0 top-0 absolute bg-stone-500 shadow" />
+      </div>
+      <img className="w-[54px] h-[54px] left-[171px] top-[4px] absolute" src="Tuvyo.png" />
+      <img className="w-[223px] h-56 left-[86px] top-[314px] absolute" src="Tuvyo.png" />
+      <div className="left-[162px] top-[538px] absolute"><span className="text-black text-xl font-normal font-['Inter'] leading-[80px] tracking-[4px]">Vino</span><span className="text-black text-xl font-normal font-['Inter'] leading-[80px]">s</span></div>
+      
+      <div className="flex-wrap flex ">
+
+        <div className="w-[177px] h-[263px] left-[12px] top-[644px] absolute bg-stone-400 rounded-[10px]">
+            <div className="w-[119px] h-[143px] left-[29px] top-[16px] absolute bg-stone-400 rounded-[14px] border border-rose-950" />
+            <img className="w-[92px] h-[121px] left-[43px] top-[27px] absolute" src="Tuvyos.png" />
+            <div className="left-[29px] top-[168px] absolute text-black text-[13px] font-normal font-['Inter'] leading-[17px] tracking-[2.60px]">Ricardo Santos</div>
+            <div className="w-[134px] h-[41px] left-[29px] top-[195px] absolute text-black text-[10px] font-normal font-['Inter'] leading-3">Malbec, Con un gran aroma y calidad en cada sorbo</div>
+            <div className="w-[68px] h-[17px] left-[89px] top-[232px] absolute">
+                <div className="w-[68px] h-[17px] left-0 top-0 absolute bg-rose-950 rounded-[30px]" />
+                <button className="w-[39.71px] h-2 left-[16px] top-[1px] absolute text-stone-400 text-[10px] font-normal font-['Inter'] leading-3">Ver mas</button>
+            </div>
+        </div>
+        <div className="w-[177px] h-[263px] left-[205px] top-[1218px] absolute bg-stone-400 rounded-[10px]">
+            <div className="w-[119px] h-[143px] left-[29px] top-[16px] absolute bg-stone-400 rounded-[14px] border border-rose-950" />
+            <img className="w-[92px] h-[121px] left-[43px] top-[27px] absolute" src="Tuvyos.png" />
+            <div className="left-[29px] top-[168px] absolute text-black text-[13px] font-normal font-['Inter'] leading-[17px] tracking-[2.60px]">Ricardo Santos</div>
+            <div className="w-[134px] h-[41px] left-[29px] top-[195px] absolute text-black text-[10px] font-normal font-['Inter'] leading-3">Malbec, Con un gran aroma y calidad en cada sorbo</div>
+            <div className="w-[68px] h-[17px] left-[89px] top-[232px] absolute">
+                <div className="w-[68px] h-[17px] left-0 top-0 absolute bg-rose-950 rounded-[30px]" />
+                <button className="w-[39.71px] h-2 left-[16px] top-[1px] absolute text-stone-400 text-[10px] font-normal font-['Inter'] leading-3">Ver mas</button>
+            </div>
+        </div>
+        <div className="w-[177px] h-[263px] left-[12px] top-[1505px] absolute bg-stone-400 rounded-[10px]">
+          <div className="w-[119px] h-[143px] left-[29px] top-[16px] absolute bg-stone-400 rounded-[14px] border border-rose-950" />
+            <img className="w-[92px] h-[121px] left-[43px] top-[27px] absolute" src="Tuvyos.png" />
+            <div className="left-[29px] top-[168px] absolute text-black text-[13px] font-normal font-['Inter'] leading-[17px] tracking-[2.60px]">Ricardo Santos</div>
+            <div className="w-[134px] h-[41px] left-[29px] top-[195px] absolute text-black text-[10px] font-normal font-['Inter'] leading-3">Malbec, Con un gran aroma y calidad en cada sorbo</div>
+            <div className="w-[68px] h-[17px] left-[89px] top-[232px] absolute">
+              <div className="w-[68px] h-[17px] left-0 top-0 absolute bg-rose-950 rounded-[30px]" />
+                <div className="w-[39.71px] h-2 left-[16px] top-[1px] absolute text-stone-400 text-[10px] font-normal font-['Inter'] leading-3">Ver mas</div>
+            </div>
+        </div>
+        <div className="w-[177px] h-[263px] left-[12px] top-[1218px] absolute bg-stone-400 rounded-[10px]">
+            <div className="w-[119px] h-[143px] left-[29px] top-[16px] absolute bg-stone-400 rounded-[14px] border border-rose-950" />
+            <img className="w-[92px] h-[121px] left-[43px] top-[27px] absolute" src="Tuvyos.png" />
+            <div className="left-[29px] top-[168px] absolute text-black text-[13px] font-normal font-['Inter'] leading-[17px] tracking-[2.60px]">Ricardo Santos</div>
+            <div className="w-[134px] h-[41px] left-[29px] top-[195px] absolute text-black text-[10px] font-normal font-['Inter'] leading-3">Malbec, Con un gran aroma y calidad en cada sorbo</div>
+            <div className="w-[68px] h-[17px] left-[89px] top-[232px] absolute">
+                <div className="w-[68px] h-[17px] left-0 top-0 absolute bg-rose-950 rounded-[30px]" />
+                <button className="w-[39.71px] h-2 left-[16px] top-[1px] absolute text-stone-400 text-[10px] font-normal font-['Inter'] leading-3">Ver mas</button>
+            </div>
+        </div>
+        <div className="w-[177px] h-[263px] left-[205px] top-[1505px] absolute bg-stone-400 rounded-[10px]">
+            <div className="w-[119px] h-[143px] left-[29px] top-[16px] absolute bg-stone-400 rounded-[14px] border border-rose-950" />
+            <img className="w-[92px] h-[121px] left-[43px] top-[27px] absolute" src="Tuvyos.png" />
+            <div className="left-[29px] top-[168px] absolute text-black text-[13px] font-normal font-['Inter'] leading-[17px] tracking-[2.60px]">Ricardo Santos</div>
+            <div className="w-[134px] h-[41px] left-[29px] top-[195px] absolute text-black text-[10px] font-normal font-['Inter'] leading-3">Malbec, Con un gran aroma y calidad en cada sorbo</div>
+            <div className="w-[68px] h-[17px] left-[89px] top-[232px] absolute">
+                <div className="w-[68px] h-[17px] left-0 top-0 absolute bg-rose-950 rounded-[30px]" />
+                <button className="w-[39.71px] h-2 left-[16px] top-[1px] absolute text-stone-400 text-[10px] font-normal font-['Inter'] leading-3">Ver mas</button>
+            </div>
+        </div>
+        <div className="w-[177px] h-[263px] left-[205px] top-[644px] absolute bg-stone-400 rounded-[10px]">
+            <div className="w-[119px] h-[143px] left-[29px] top-[16px] absolute bg-stone-400 rounded-[14px] border border-rose-950" />
+            <img className="w-[92px] h-[121px] left-[43px] top-[27px] absolute" src="Tuvyos.png" />
+            <div className="left-[29px] top-[168px] absolute text-black text-[13px] font-normal font-['Inter'] leading-[17px] tracking-[2.60px]">Ricardo Santos</div>
+            <div className="w-[134px] h-[41px] left-[29px] top-[195px] absolute text-black text-[10px] font-normal font-['Inter'] leading-3">Malbec, Con un gran aroma y calidad en cada sorbo</div>
+            <div className="w-[68px] h-[17px] left-[89px] top-[232px] absolute">
+                <div className="w-[68px] h-[17px] left-0 top-0 absolute bg-rose-950 rounded-[30px]" />
+                <button className="w-[39.71px] h-2 left-[16px] top-[1px] absolute text-stone-400 text-[10px] font-normal font-['Inter'] leading-3">Ver mas</button>
+            </div>
+        </div>
+        <div className="w-[177px] h-[263px] left-[205px] top-[931px] absolute bg-stone-400 rounded-[10px]">
+            <div className="w-[119px] h-[143px] left-[29px] top-[16px] absolute bg-stone-400 rounded-[14px] border border-rose-950" />
+            <img className="w-[92px] h-[121px] left-[43px] top-[27px] absolute" src="Tuvyos.png" />
+            <div className="left-[29px] top-[168px] absolute text-black text-[13px] font-normal font-['Inter'] leading-[17px] tracking-[2.60px]">Ricardo Santos</div>
+            <div className="w-[134px] h-[41px] left-[29px] top-[195px] absolute text-black text-[10px] font-normal font-['Inter'] leading-3">Malbec, Con un gran aroma y calidad en cada sorbo</div>
+            <div className="w-[68px] h-[17px] left-[89px] top-[232px] absolute">
+                <div className="w-[68px] h-[17px] left-0 top-0 absolute bg-rose-950 rounded-[30px]" />
+                <button className="w-[39.71px] h-2 left-[16px] top-[1px] absolute text-stone-400 text-[10px] font-normal font-['Inter'] leading-3">Ver mas</button>
+            </div>
+        </div>
+        <div className="w-[177px] h-[263px] left-[12px] top-[931px] absolute bg-stone-400 rounded-[10px]">
+            <div className="w-[119px] h-[143px] left-[29px] top-[16px] absolute bg-stone-400 rounded-[14px] border border-rose-950" />
+            <img className="w-[92px] h-[121px] left-[43px] top-[27px] absolute" src="Tuvyos.png" />
+            <div className="left-[29px] top-[168px] absolute text-black text-[13px] font-normal font-['Inter'] leading-[17px] tracking-[2.60px]">Ricardo Santos</div>
+            <div className="w-[134px] h-[41px] left-[29px] top-[195px] absolute text-black text-[10px] font-normal font-['Inter'] leading-3">Malbec, Con un gran aroma y calidad en cada sorbo</div>
+            <div className="w-[81px] h-[18px] left-[75px] top-[227px] absolute">
+                <div className="w-[81px] h-[18px] left-0 top-0 absolute bg-rose-950 rounded-[30px]" />
+                <button className="w-[47.30px] h-[8.44px] left-[19.06px] top-[1.06px] absolute text-white text-[10px] font-normal font-['Inter'] leading-3">Ver mas</button>
+            </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+  </div>
+  </div>
   )
 }
